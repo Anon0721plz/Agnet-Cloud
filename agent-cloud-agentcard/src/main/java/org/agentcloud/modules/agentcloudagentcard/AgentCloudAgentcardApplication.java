7f4c3581688ade013552dbfe9a574c587f9ef66f
ygr <1210911688@qq.com>
2026-08-03 09:13:48 +0800

    feat:新增agentcard模块

---
+++ agent-cloud-agentcard/src/main/java/org/agentcloud/modules/agentcloudagentcard/AgentCloudAgentcardApplication.java
@@ -0,0 +1,13 @@
+package org.agentcloud.modules.agentcloudagentcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentCloudAgentcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentCloudAgentcardApplication.class, args);
+    }
+
+}
